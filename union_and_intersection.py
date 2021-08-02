115052d16d0993ad472f46577f9e63397d93b1df
Dmitri-dkhd <83269510+Dmitri-dkhd@users.noreply.github.com>
2021-08-02 10:25:36 +0200

    Update union_and_intersection.py

--- union_and_intersection.py
+++ union_and_intersection.py
@@ -76,7 +76,6 @@ def intersection(llist_1, llist_2):
         head_2 = head_2.next
     return intersection_llist
 
-
 # Test case 1
 
 linked_list_1 = LinkedList()
@@ -91,8 +90,8 @@ for i in element_1:
 for i in element_2:
     linked_list_2.append(i)
 
-print(union(linked_list_1, linked_list_2))
-print(intersection(linked_list_1, linked_list_2))
+print(union(linked_list_1, linked_list_2)) #returns 3 -> 2 -> 4 -> 35 -> 6 -> 65 -> 21 -> 32 -> 9 -> 1 -> 11 -> 
+print(intersection(linked_list_1, linked_list_2)) #returns 6 -> 4 -> 21 -> 
 
 # Test case 2
 
@@ -108,5 +107,22 @@ for i in element_1:
 for i in element_2:
     linked_list_4.append(i)
 
-print(union(linked_list_3, linked_list_4))
-print(intersection(linked_list_3, linked_list_4))
+print(union(linked_list_3, linked_list_4)) #returns 3 -> 2 -> 4 -> 35 -> 6 -> 65 -> 23 -> 1 -> 7 -> 8 -> 9 -> 11 -> 21 ->
+print(intersection(linked_list_3, linked_list_4)) #returns nothing
+
+# Test case 3
+
+linked_list_5 = LinkedList()
+linked_list_6 = LinkedList()
+
+element_1 = list(range(100))
+element_2 = []
+
+for i in element_1:
+    linked_list_5.append(i)
+
+for i in element_2:
+    linked_list_6.append(i)
+
+print(union(linked_list_5, linked_list_6)) #prints numbers 0 to 99
+print(intersection(linked_list_5, linked_list_6)) #returns nothing
